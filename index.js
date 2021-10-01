89328ffa5b50eee82bb43c90f1967bd56908c67a
laeljd <lael.jader@gmail.com>
2021-10-01 15:31:38 -0300

    on ready message

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const bot = new Client({
 
 bot.login("")
 
-bot.once('ready', () => { console.log(`Bot online: ${bot.user.tag}!`); });
+bot.once('ready', () => { 
+  console.log(`Bot online: ${bot.user.tag}!`); 
+});
 
 bot.on('message', msg => {
   if (msg.content === '!oi') {
-  msg.reply('Esse é o meu primeiro bot!');
+    msg.reply('Esse é o meu primeiro bot!');
   }
+});
+
+
+bot.on('ready', () => {
+  var channelName = "geral"
+  var message = "Bot iniciado"
+
+  const channel = bot.channels.cache.find(channel => channel.name === channelName )
+  
+  channel.send(message)
+  
 });
